d1c5db22a377ca2068e795503dd753e4ade8be4d
lukaszjargielo <l.jargielo88@gmail.com>
2025-03-24 09:03:23 +0100

    First parameterized test to count goals average

--- src/test/java/com/alansystems/footballstatistics/utils/MessageProcessorTest.java
+++ src/test/java/com/alansystems/footballstatistics/utils/MessageProcessorTest.java
@@ -1,27 +1,37 @@
 package com.alansystems.footballstatistics.utils;
 
 import com.alansystems.footballstatistics.model.TeamStatistics;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class MessageProcessorTest {
-    @Test
-    public void testUpdateAverageGoals() {
+
+    @ParameterizedTest
+    @CsvSource({
+            "5, 12, 8, 4.0",
+            "6, 13, 9, 3.67",
+            "2, 3, 6, 4.5",
+            "4, 9, 8, 4.25",
+            "6, 11, 15, 4.33"
+    })
+
+    public void testAverageGoals(int numberOfEventPlayed, int goalsScored, int goalsConceded, double expectedAverageGoals) {
         TeamStatistics mockTeamStatistics = mock(TeamStatistics.class);
 
-        when(mockTeamStatistics.getSumOfGoalsScored()).thenReturn(13);
-        when(mockTeamStatistics.getSumOfGoalsConceded()).thenReturn(9);
-        when(mockTeamStatistics.getNumberOfPlayedEvents()).thenReturn(6);
+        when(mockTeamStatistics.getNumberOfPlayedEvents()).thenReturn(numberOfEventPlayed);
+        when(mockTeamStatistics.getSumOfGoalsScored()).thenReturn(goalsScored);
+        when(mockTeamStatistics.getSumOfGoalsConceded()).thenReturn(goalsConceded);
 
         MessageProcessor messageProcessor = new MessageProcessor();
 
         messageProcessor.updateAverageGoals(mockTeamStatistics);
 
-        verify(mockTeamStatistics, times(1)).setAverageAmountOfGoalsInTheTeamEvents(3.67);
-
-        /*private void updateAverageGoals(TeamStatistics teamObj) {
-            double averageGoals = Math.round(((teamObj.getSumOfGoalsScored() + teamObj.getSumOfGoalsConceded()) * 1.0 / teamObj.getNumberOfPlayedEvents()) * 100.0) / 100.0;
-            teamObj.setAverageAmountOfGoalsInTheTeamEvents(averageGoals);*/
+        verify(mockTeamStatistics, times(1)).setAverageAmountOfGoalsInTheTeamEvents(expectedAverageGoals);
     }
+
 }
